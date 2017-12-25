2314057d6cd4f9b8fbf2691df93d792a98a27e83
cxm12900733 <709645492@qq.com>
2017-12-25 18:12:45 +0800

    日常

--- Website/Controllers/ManageController.cs
+++ Website/Controllers/ManageController.cs
@@ -54,41 +54,25 @@ namespace Website.Controllers
         [HttpGet]
         public ActionResult Edit(int Id)
         {
-            var Sys_Menu = this.Entity.Sys_Menu.FirstOrDefault(o => o.ID == Id);
-            if (Sys_Menu==null)
+            var Sys_Manage = this.Entity.Sys_Manage.FirstOrDefault(o => o.ID == Id);
+            if (Sys_Manage == null)
             {
                 return this.Error("数据不存在");
             }
-            this.ViewBag.Sys_Menu = Sys_Menu;
-            //图标文件
-            string filename = this.Server.MapPath("/Icon.json");
-            string jsonstr = System.IO.File.ReadAllText(filename);
-            var ComboboxModelList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<ComboboxModel>>(jsonstr);
-            var CustomIconListJson = Newtonsoft.Json.JsonConvert.SerializeObject(ComboboxModelList.OrderBy(o => o.group).ToList());
-            this.ViewBag.CustomIconListJson = CustomIconListJson;
+            this.ViewBag.Sys_Manage = Sys_Manage;
+
+            var Sys_RoleList = this.Entity.Sys_Role.AsNoTracking().Where(o => o.State == State.Normal).OrderBy(o => o.Sort).ToList();
+            this.ViewBag.Sys_RoleList = Sys_RoleList;
             return View();
         }
 
         [HttpPost]
-        public ActionResult Edit(Sys_Menu Sys_Menu)
+        public ActionResult Edit(Sys_Manage Sys_Manage)
         {
-            var baseSys_Menu = this.Entity.Sys_Menu.FirstOrDefault(o => o.ID == Sys_Menu.ID);
-            if (baseSys_Menu == null)
+            var baseSys_Manage = this.Entity.Sys_Manage.FirstOrDefault(o => o.ID == Sys_Manage.ID);
+            if (baseSys_Manage == null)
             {
                 return this.Error("数据不存在");
-            }
-            baseSys_Menu = this.Request.ConvertRequestToModel<Sys_Menu>(baseSys_Menu,Sys_Menu);
-            switch (baseSys_Menu.MenuType)
-            {
-                case MenuType.Directory:
-                    baseSys_Menu.Control = string.Empty;
-                    baseSys_Menu.Method = string.Empty;
-                    baseSys_Menu.Url = string.Empty;
-                    break;
-                case MenuType.Powers:
-                    baseSys_Menu.Url = string.Empty;
-                    baseSys_Menu.Icon = string.Empty;
-                    break;
             }
             this.Entity.SaveChanges();
             return this.Succeed("操作成功");
@@ -99,9 +83,9 @@ namespace Website.Controllers
         {
             var Result = new AjaxResult();
             var idsStrArr = ids.Split(',');
-            int[] idsArr = Array.ConvertAll<string, int>(idsStrArr, s => int.Parse(s));  
-            var baseSys_Menu = this.Entity.Sys_Menu.Where(o => idsArr.Contains(o.ID)).ToList();
-            this.Entity.Sys_Menu.RemoveRange(baseSys_Menu);
+            int[] idsArr = Array.ConvertAll<string, int>(idsStrArr, s => int.Parse(s));
+            var baseSys_Manage = this.Entity.Sys_Manage.Where(o => idsArr.Contains(o.ID)).ToList();
+            this.Entity.Sys_Manage.RemoveRange(baseSys_Manage);
             this.Entity.SaveChanges();
             Result.Message = "操作成功";
             return this.ToJson(Result);
---
+++ Website/Models/EasyUI/DataListModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models.EasyUI
+{
+    public class DataListModel
+    {
+        /// <summary>
+        /// 文本
+        /// </summary>
+        public string text { get; set; }
+        /// <summary>
+        /// 值
+        /// </summary>
+        public string value { get; set; } 
+        /// <summary>
+        /// 选中
+        /// </summary>
+        public bool @checked {get;set;}
+    }
+}
