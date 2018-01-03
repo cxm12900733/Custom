6838d7eed5ed75a69b69489f987828f57fded173
cxm12900733 <709645492@qq.com>
2018-01-04 06:51:01 +0800

    对称加密码

--- Website/Controllers/HomeController.cs
+++ Website/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 namespace Website.Controllers
@@ -12,8 +15,110 @@ namespace Website.Controllers
 
         public ActionResult Index()
         {
+            string original = "Here is some data to encrypt!";
+            string key = "AES.Custom.Web.Manage.Login.ABCD";
+            var keyarr = Encoding.ASCII.GetBytes(key);
+            AesManaged myAes = new AesManaged();
+            //myAes.Key = keyarr;
+            //myAes.GenerateIV();
+            //var Istrue = myAes.ValidKeySize(myAes.Key.Length);
+
+            ICryptoTransform encryptor = myAes.CreateEncryptor();
+            byte[] encrypted;
+            string str = Encrypt(original, encryptor);
+            //using (MemoryStream msEncrypt = new MemoryStream())
+            //{
+            //    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+            //    {
+            //        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+            //        {
+
+            //            //Write all data to the stream.
+            //            swEncrypt.Write(original);
+            //        }
+
+            //        encrypted = msEncrypt.ToArray();
+            //    }
+            //}
+            //string str = Encoding.Default.GetString(encrypted);
+
+            var newEncrypted = Convert.FromBase64String(str);
+            string neworiginal = string.Empty;
+            ICryptoTransform decryptor = myAes.CreateDecryptor();
+            // Create the streams used for decryption.
+            using (MemoryStream msDecrypt = new MemoryStream(newEncrypted))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+
+                        // Read the decrypted bytes from the decrypting stream
+                        // and place them in a string.
+                        neworiginal = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+
             return View();
         }
+
+        // 加密算法
+        private string Encrypt(string clearText,ICryptoTransform encryptor)
+        {
+            // 创建明文流
+            byte[] clearBuffer = Encoding.UTF8.GetBytes(clearText);
+            MemoryStream clearStream = new MemoryStream(clearBuffer);
+
+            // 创建空的密文流
+            MemoryStream encryptedStream = new MemoryStream();
+
+            CryptoStream cryptoStream =
+                new CryptoStream(encryptedStream, encryptor, CryptoStreamMode.Write);
+
+            // 将明文流写入到buffer中
+            // 将buffer中的数据写入到cryptoStream中
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            do
+            {
+                bytesRead = clearStream.Read(buffer, 0, 1024);
+                cryptoStream.Write(buffer, 0, bytesRead);
+            } while (bytesRead > 0);
+
+            cryptoStream.FlushFinalBlock();
+
+            // 获取加密后的文本
+            buffer = encryptedStream.ToArray();
+            string encryptedText = Convert.ToBase64String(buffer);
+            return encryptedText;
+        }
+
+        // 解密算法
+        //private string Decrypt(string encryptedText)
+        //{
+        //    byte[] encryptedBuffer = Convert.FromBase64String(encryptedText);
+        //    Stream encryptedStream = new MemoryStream(encryptedBuffer);
+
+        //    MemoryStream clearStream = new MemoryStream();
+        //    CryptoStream cryptoStream =
+        //        new CryptoStream(encryptedStream, decryptor, CryptoStreamMode.Read);
+
+        //    int bytesRead = 0;
+        //    byte[] buffer = new byte[BufferSize];
+
+        //    do
+        //    {
+        //        bytesRead = cryptoStream.Read(buffer, 0, BufferSize);
+        //        clearStream.Write(buffer, 0, bytesRead);
+        //    } while (bytesRead > 0);
+
+        //    buffer = clearStream.GetBuffer();
+        //    string clearText =
+        //        Encoding.UTF8.GetString(buffer, 0, (int)clearStream.Length);
+
+        //    return clearText;
+        //}
 
         //public void test()
         //{
